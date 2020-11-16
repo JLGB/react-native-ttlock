a973b3daf2c36138a2d5a01e044e236ecacfe4dd
Sciener <513466979@qq.com>
2020-11-16 12:06:35 +0800

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,9 +2,14 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ScanLockPage from './ScanLockPage';
-import MainPage from "./MainPage"
-import LockPage from './LockPage';
+import ScanLockPage from './scan-lock-page';
+import MainPage from "./main-page"
+import LockPage from './lock-page';
+import ScanGatewayPage from './scan-gateway-page'
+import GatewayPage from './gateway-page'
+import ScanWifiPage from './scan-wifi-page'
+
+
 
 
 
@@ -13,11 +18,14 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LockPage">
+      <Stack.Navigator initialRouteName="MainPage">
         <Stack.Screen  name="MainPage" component={MainPage} options={{title:"Ttlock Demo"}} />
         <Stack.Screen name="ScanLockPage" component={ScanLockPage} options={{title:"Lock"}} />
+        <Stack.Screen name="ScanGatewayPage" component={ScanGatewayPage} options={{title:"Gateway"}} />
         {/* <Stack.Screen name="LockPage" component={LockPage} options={({ route }) => ({ title: route.params.scanLockMap.lockName })} /> */}
         <Stack.Screen name="LockPage" component={LockPage}  />
+        <Stack.Screen name="GatewayPage" component={GatewayPage}  />
+        <Stack.Screen name="ScanWifiPage" component={ScanWifiPage}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
